1b98851b6258039b8a4165cdc03423d835784aa9
Andrei <andrei.lisneuski@gmail.com>
2023-03-19 18:24:26 -0400

    Termine la classe Mot

--- TP2_Depart/TP2_Depart/src/Mot.java
+++ TP2_Depart/TP2_Depart/src/Mot.java
@@ -1,22 +1,96 @@
 // VOUS POUVEZ MODIFIER CE FICHIER
+import java.util.Vector;
 
 public class Mot {
-    private char[] lettres;
+    private Vector lettres;
     private int nbLettres;
 
     public Mot suivant = null;
 
     public Mot() {
-        lettres = new char[0];
+        lettres = new Vector();
         nbLettres = 0;
     }
 
     public Mot(String str) {
         assert str.indexOf(' ') == -1 : "Un mot ne peut contenir d'espaces";
-        lettres = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            lettres.add(c);
+        }
+
         nbLettres = str.length();
     }
 
     // AJOUTEZ VOTRE CODE CI-DESSOUS
+
+
+    public String toString(){
+        String word = "";
+        for (int i = 0; i < lettres.size(); i++) {
+            char c = (char) lettres.get(i);
+            word += c;
+        }
+        return word;
+    }
+
+    public int getLongueur(){
+        return nbLettres;
+    }
+
+    public char getLettre(int index){
+        if (index < 0 || index >= nbLettres) {
+            return '0';
+        }
+        return (char) lettres.get(index);
+    }
+
+    public void ajouter(char lettre){
+        int currentSize = lettres.size();
+        int newSize = currentSize * 2 + 1;
+        while (newSize > lettres.capacity()) {
+            lettres.ensureCapacity(lettres.capacity() * 2);
+        }
+        lettres.add(lettre);
+        nbLettres += 1;
+        lettres.trimToSize();
+    }
+
+
+
+    public boolean inserer(char lettre, int index){
+        if (index < 0 || index >= nbLettres) {
+            return false;
+        }
+        int currentSize = lettres.size();
+        int newSize = currentSize * 2 + 1;
+        while (newSize > lettres.capacity()) {
+            lettres.ensureCapacity(lettres.capacity() * 2);
+        }
+        lettres.add(index, lettre);
+        nbLettres += 1;
+        lettres.trimToSize();
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
